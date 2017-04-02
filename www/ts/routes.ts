e815a6d5983e98be883c07d419ea63a1b491ddb0
Ruslan López <scherzo_16@hotmail.com>
2017-04-02 18:19:55 -0500

    Se modifica para compila al guardar

---
+++ www/ts/routes.ts
@@ -0,0 +1,43 @@
+angular.module("proyecto").config(
+    function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise("/perfil");
+
+        return $stateProvider.state("dashboard", {
+            url: "/dashboard",
+            templateUrl: "templates/dashboard.html"
+            //,controller: "CntrlDashboard"
+        })
+            .state("perfil", {
+                url: "/perfil",
+                templateUrl: "templates/perfil.html"
+            })
+            .state("settings", {
+                url: "/settings",
+                templateUrl: "templates/settings.html"
+            })
+            .state("score", {
+                url: "/score",
+                templateUrl: "templates/score.html"
+            })
+            .state("perfil_curso", {
+                url: "/perfil_curso",
+                templateUrl: "templates/perfil_curso.html"
+            })
+            .state("login", {
+                url: "/login",
+                templateUrl: "templates/login.html"
+            })
+            .state("register", {
+                url: "/register",
+                templateUrl: "templates/register.html"
+            })
+            .state("register_2", {
+                url: "/register_2",
+                templateUrl: "templates/register_2.html"
+            })
+            .state("register_3", {
+                url: "/register_3",
+                templateUrl: "templates/register_3.html"
+            });
+    }
+);
