4277f5c91d5749a226ae61e3bc87da932a305d57
Ruslan López <scherzo_16@hotmail.com>
2017-04-02 20:29:15 -0500

    APP

---
+++ www/ts/controllers/app.ts
@@ -0,0 +1,12 @@
+var app = angular.module('proyecto', ['ionic', 'app.controllers'])
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+
+});
